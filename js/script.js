434e1f30dcf32a2b35cf8a4dc86da413dcd8d7c4
DevGuiMuniz <guigugui16@gmail.com>
2024-06-21 21:12:13 -0300

    uptdate 21/06

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
     const imgElement = document.querySelector('.apresentacao_img');
     imgElement.classList.add('animar');
+
   });
+
+  
+  // Obtém a imagem pelo ID
+  const image = document.getElementById('myImage');
+  const image2 = document.getElementById('myImage2');
+  const image3 = document.getElementById('myImage3');
+  const image4 = document.getElementById('myImage4');
+  const img5 = document.getElementById("ecofuryimg");
+
+
+  // Adiciona a classe 'shadow' à imagem
+  img5.classList.add('shadow1');
+  image.classList.add('shadow');
+  image2.classList.add('shadow');
+  image3.classList.add('shadow');
+  image4.classList.add('shadow');
+
 
   
